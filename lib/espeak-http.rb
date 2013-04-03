9cbcdcb645395b92fab897b03b83fd637a01f772
Martin C. Richards <martin.charles.richards@gmail.com>
2013-04-03 23:04:28 +0200

    Added page to interact directly

--- lib/espeak-http.rb
+++ lib/espeak-http.rb
@@ -1,16 +1,32 @@
-%w(rubygems 
-   sinatra 
+%w(rubygems
+   sinatra
    espeak-ruby
    digest/sha1).each { |l| require l }
 
 include ESpeak
+# use Rack::Static, :urls => ['/tmp'],
+#   :root => '/home/martin/code/espeak-http/tmp'
+
 
 get '/' do
-  redirect 'http://rors.org/2010/10/21/espeak-ruby.html', 301
+  erb :index
 end
 
 get '/tts' do
-  filename = "tmp/#{Digest::SHA1.hexdigest(params.to_s)}.mp3"
-  espeak(filename, params) # unless filename exists
-  [200, {'Content-type' => 'audio/mpeg'}, File.read(filename)]
+  erb :index
+end
+
+post '/tts' do
+  if params[:text]
+    filename = "/tmp/#{Digest::SHA1.hexdigest(params.to_s)}.mp3"
+    espeak(filename, params) # unless filename exists
+    @filename = filename
+  end
+
+  erb :index
+end
+
+get '/tmp/:filename' do
+  [200, {'Content-type' => 'audio/mpeg'},
+    File.read("/tmp/#{params[:filename]}")]
 end
