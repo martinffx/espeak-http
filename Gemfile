ee4cd151043ab191acbd43f25e837ee3f02ca87a
Martin C. Richards <martin.charles.richards@gmail.com>
2014-06-22 17:25:20 +0200

    clean up

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'espeak-ruby'
+
+group :development do
+  gem 'pry-byebug'
+end
--- lib/espeak-http.rb
+++ lib/espeak-http.rb
@@ -4,22 +4,11 @@
    digest/sha1).each { |l| require l }
 
 include ESpeak
-# use Rack::Static, :urls => ['/tmp'],
-#   :root => '/home/martin/code/espeak-http/tmp'
-
 
 get '/' do
-  erb :index
-end
-
-get '/tts' do
-  erb :index
-end
-
-post '/tts' do
   if params[:text]
     filename = "/tmp/#{Digest::SHA1.hexdigest(params.to_s)}.mp3"
-    espeak(filename, params) # unless filename exists
+    espeak(filename, params) unless File.exists? filename
     @filename = filename
   end
 
